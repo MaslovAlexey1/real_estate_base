baa44254da39339f8181af63fafd9665a58a241f
MaslovAlexey1 <alexey.maslov@dvigus.ru>
2019-10-11 20:55:15 +0300

    add basic plots

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,31 @@
-from flask import Flask, escape, request, render_template, flash, redirect, url_for
+from matplotlib.ticker import FuncFormatter
+from flask import Flask, escape, request, render_template, flash, redirect, url_for, send_file
 from app import app
 from app.forms import LoginForm
 from app.models import Developer, HousingComplex, House, Object
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from io import StringIO
+import io
+import seaborn as sns
+sns.set()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan2'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+	user = {'username': 'Miguel'}
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username': 'Susan2'},
+			'body': 'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -29,7 +37,6 @@ def login():
 	return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/real_estate_base/')
-# @app.route('/real_estate_base/?dev=<developer_id>')
 def real_estate_base():
 	# return 'DEVELOPER_ID%s' %developer_id
 	developer = Developer.query.all()
@@ -67,8 +74,77 @@ def real_estate_base():
 		object = object
 		)
 
+@app.route('/fig')
 
+def fig():
+	developer_id = request.args.get('dev')
+	developer_id = int(developer_id) if developer_id is not None else 0
+
+	housing_complex_id = request.args.get('hc')
+	housing_complex_id = int(housing_complex_id) if housing_complex_id is not None else 0
+
+	house_id = request.args.get('h')
+	house_id = int(house_id) if house_id is not None else 0
+	
+	fig, ax = plt.subplots(figsize=(10, 6))
+
+	conn = 'sqlite:///app.db'
+	if developer_id == 0:
+		# df = pd.read_sql('select d.id, d.name, min(o.price) min_price, max(o.price) max_price, count(o.id) objects from object o join developer d on o.developer_id=d.id group by d.id', conn)
+		df = pd.read_sql('select o.room, o.square, o.price from object o order by random() limit 5000', conn)
+		df['price'] = pd.to_numeric(df['price'])
+		df['square'] = pd.to_numeric(df['square'])
+
+		g = sns.scatterplot(x='square', y='price', data=df)
+		plt.tight_layout()
+
+		formatter = FuncFormatter(millions)
+		g.yaxis.set_major_formatter(formatter)
+	elif housing_complex_id == 0:
+		df = pd.read_sql('select hc.id, hc.name, o.price from object o join housing_complex hc on o.housing_complex_id=hc.id where o.developer_id = ' + str(developer_id), conn)
+		df['price'] = pd.to_numeric(df['price'])
+		g = sns.boxplot(y='price', x='name', data=df)
+		g.set_xticklabels(g.get_xticklabels(), rotation=45,
+		                  horizontalalignment='right')
+		plt.tight_layout()
+
+		g.set_xlabel('')
+		g.set_ylabel('')
+
+		formatter = FuncFormatter(millions)
+		g.yaxis.set_major_formatter(formatter)
+	elif house_id == 0:
+		df = pd.read_sql(
+			'select h.id, h.name, o.price from object o join house h on o.house_id=h.id where o.housing_complex_id = ' + str(housing_complex_id), conn)
+		df['price'] = pd.to_numeric(df['price'])
+		g = sns.boxplot(y='price', x='name', data=df)
+		g.set_xticklabels(g.get_xticklabels(), rotation=45,
+		                  horizontalalignment='right')
+		plt.tight_layout()
+
+		g.set_xlabel('')
+		g.set_ylabel('')
+
+		formatter = FuncFormatter(millions)
+		g.yaxis.set_major_formatter(formatter) 
+	elif house_id != 0:
+		df = pd.read_sql('select * from object where house_id = ' + str(house_id), conn)
+
+	# ax.hist(np.random.rand(Ntotal), 20)
+
+	img = io.BytesIO()
+	fig.savefig(img)
+	img.seek(0)
+	return send_file(img, mimetype='image/png')
+
+
+def millions(x, pos):
+    'The two args are the value and tick position'
+    return '%1.0fM' % (x/1e6)
 
 @app.route('/about')
 def about():
 	return 'The about page'
+
+
+
